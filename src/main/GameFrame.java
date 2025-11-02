b986e57e502cf6ebd22f7f6961be4408d4ecb23b
Jody <jodymarcialis@gmail.com>
2025-11-02 19:28:48 +0100

    Add files via upload

---
+++ src/main/GameFrame.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+
+	public GameFrame() {
+		this.add(new GamePanel()); // 2nd raccourci
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
---
+++ src/main/SnakeGame.java
@@ -0,0 +1,21 @@
+package main;
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		/* TODO : à implémenter pour la version alpha
+		 * 
+		 * une deuxième couleur de pomme: doré, généré aléatoirement
+		 * (mais les pommes rouges restent les plus communes)
+		 * 
+		 * si le serpent mange une pomme dorée, il change de couleur 
+		 * jusqu'à ce qu'il mange une pomme normale
+		 * 
+		 * implémenter un AudioPlayer pour avoir un BGM et un effet sonore au GameOver
+		 * au moment de commencer, le volume sonore du BGM croit de 0 au max (rapidement si possible)
+		 * 
+		 */
+		
+		new GameFrame(); // raccourci, où la Frame n'est pas nommée 
+	}
+}
